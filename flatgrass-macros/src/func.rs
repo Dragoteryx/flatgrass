35ad0a9797260f0b5a6ee070e5059f2374f87a8f
Dragoteryx <dragoteryx@live.fr>
2025-08-28 21:13:46 +0200

    more macro improvements

--- flatgrass-macros/src/func.rs
+++ flatgrass-macros/src/func.rs
@@ -123,69 +123,66 @@ pub fn generate_func(func: &ItemFn) -> TokenStream {
 	};
 
 	if let Some(unsafety) = &func.sig.unsafety {
-		errors.push(
-			quote_spanned!(unsafety.span => compile_error!("Lua functions cannot be unsafe")),
-		);
+		errors.push(quote_spanned! { unsafety.span =>
+			compile_error!("Lua functions cannot be unsafe")
+		});
 	}
 
 	if let Some(asyncness) = &func.sig.asyncness {
-		errors.push(
-			quote_spanned!(asyncness.span => compile_error!("Lua functions cannot be async (yet)")),
-		);
+		errors.push(quote_spanned! { asyncness.span =>
+			compile_error!("Lua functions cannot be async (yet)")
+		});
 	}
 
-	let body = if !errors.is_empty() {
-		quote! { ::flatgrass::ffi::raw_function!(|_| 0) }
-	} else {
-		let args = func.sig.inputs.iter().map(|input| {
-			quote_spanned! { input.span() =>
-				match ::flatgrass::lua::traits::LuaFnParam::lua_fn_param(lua, &mut arg, &mut upv) {
-					::core::result::Result::Ok(value) => value,
-					::core::result::Result::Err(err) => {
-						lua.stack().clear();
-						lua.stack().push_any(err);
-						return ::core::option::Option::None;
+	let body = match errors.is_empty() {
+		false => quote! { ::flatgrass::ffi::raw_function!(|_| 0) },
+		true => {
+			let args = func.sig.inputs.iter().map(|input| {
+				quote_spanned! { input.span() =>
+					match ::flatgrass::lua::traits::LuaFnParam::lua_fn_param(lua, &mut arg, &mut upv) {
+						::core::result::Result::Ok(value) => value,
+						::core::result::Result::Err(err) => {
+							lua.stack().clear();
+							lua.stack().push_any(err);
+							return ::core::option::Option::None;
+						}
 					}
 				}
-			}
-		});
+			});
 
-		let call = if func.sig.asyncness.is_some() {
-			quote_spanned! { ret_span =>
-
-
-			}
-		} else {
-			quote_spanned! { ret_span =>
-				match ::flatgrass::lua::traits::LuaFnReturn::lua_fn_return(#ident #generics_turbofish (#(#args),*), lua) {
-					::core::result::Result::Ok(::flatgrass::lua::traits::Return::Values(values)) =>
-						::core::option::Option::Some(::flatgrass::lua::traits::Return::Values(lua.stack().push_many(values))),
-					::core::result::Result::Ok(::flatgrass::lua::traits::Return::Yield(values)) =>
-						::core::option::Option::Some(::flatgrass::lua::traits::Return::Yield(lua.stack().push_many(values))),
-					::core::result::Result::Err(err) => {
-						lua.stack().clear();
-						lua.stack().push_any(err);
-						::core::option::Option::None
+			let call = match &func.sig.asyncness {
+				Some(_) => quote_spanned! { ret_span => },
+				None => quote_spanned! { ret_span =>
+					match ::flatgrass::lua::traits::LuaFnReturn::lua_fn_return(#ident #generics_turbofish (#(#args),*), lua) {
+						::core::result::Result::Ok(::flatgrass::lua::traits::Return::Values(values)) =>
+							::core::option::Option::Some(::flatgrass::lua::traits::Return::Values(lua.stack().push_many(values))),
+						::core::result::Result::Ok(::flatgrass::lua::traits::Return::Yield(values)) =>
+							::core::option::Option::Some(::flatgrass::lua::traits::Return::Yield(lua.stack().push_many(values))),
+						::core::result::Result::Err(err) => {
+							lua.stack().clear();
+							lua.stack().push_any(err);
+							::core::option::Option::None
+						}
 					}
 				}
-			}
-		};
+			};
 
-		quote! {
-			pub unsafe extern "C-unwind" fn func #impl_generics (state: *mut ::flatgrass::ffi::lua_State) -> ::flatgrass::ffi::libc::c_int #where_clause {
-				match ::flatgrass::lua::Lua::init(state, |lua| {
-					let (mut arg, mut upv) = (1, 1);
-					#call
-				}) {
-					::core::option::Option::None => ::flatgrass::ffi::lua_error(state),
-					::core::option::Option::Some(ret) => match ret {
-						::flatgrass::lua::traits::Return::Yield(i) => ::flatgrass::ffi::lua_yield(state, i),
-						::flatgrass::lua::traits::Return::Values(i) => i,
+			quote! {
+				pub unsafe extern "C-unwind" fn func #impl_generics (state: *mut ::flatgrass::ffi::lua_State) -> ::flatgrass::ffi::libc::c_int #where_clause {
+					match ::flatgrass::lua::Lua::init(state, |lua| {
+						let (mut arg, mut upv) = (1, 1);
+						#call
+					}) {
+						::core::option::Option::None => ::flatgrass::ffi::lua_error(state),
+						::core::option::Option::Some(ret) => match ret {
+							::flatgrass::lua::traits::Return::Yield(i) => ::flatgrass::ffi::lua_yield(state, i),
+							::flatgrass::lua::traits::Return::Values(i) => i,
+						}
 					}
 				}
-			}
 
-			func::<#type_generics>
+				func::<#type_generics>
+			}
 		}
 	};
 
