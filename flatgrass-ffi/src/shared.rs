1c7dd2422e6fb67a8c42fcf41f687e612598b812
Dragoteryx <dragoteryx@live.fr>
2026-07-28 21:56:02 +0200

    update libloading + use opened lib on windows

--- flatgrass-ffi/src/shared.rs
+++ flatgrass-ffi/src/shared.rs
@@ -1,59 +1,65 @@
 use libloading::Library;
-use std::path::Path;
 use std::sync::LazyLock;
 
+#[cfg(any(fg_win32, fg_win64))]
+use libloading::os::windows::Library as WindowsLibrary;
+
 #[allow(unused_macros)]
-macro_rules! find_lua_path {
-	($($path:literal),+ $(,)?) => {
-		[ $($path),+ ]
-			.into_iter()
-			.map(Path::new)
-			.find(|path| path.exists())
+macro_rules! open_library {
+	($path:literal) => {
+		Library::new($path).ok()
 	};
 }
 
+#[cfg(any(fg_win32, fg_win64))]
+fn opened_lua_shared() -> Option<Library> {
+	WindowsLibrary::open_already_loaded("lua_shared.dll")
+		.map(Library::from)
+		.ok()
+}
+
 #[cfg(fg_win32)]
-fn lua_path() -> Option<&'static Path> {
-	find_lua_path! {
-		"garrysmod/bin/lua_shared.dll",
-		"bin/lua_shared.dll",
+fn find_lua_shared() -> Option<Library> {
+	unsafe {
+		opened_lua_shared()
+			.or_else(|| open_library!("bin/lua_shared.dll"))
+			.or_else(|| open_library!("garrysmod/bin/lua_shared.dll"))
 	}
 }
 
 #[cfg(fg_win64)]
-fn lua_path() -> Option<&'static Path> {
-	find_lua_path! {
-		"bin/win64/lua_shared.dll",
+fn find_lua_shared() -> Option<Library> {
+	unsafe {
+		opened_lua_shared()
+			.or_else(|| open_library!("bin/win64/lua_shared.dll"))
 	}
 }
 
 #[cfg(fg_linux32)]
-fn lua_path() -> Option<&'static Path> {
-	find_lua_path! {
-		"garrysmod/bin/lua_shared_srv.so",
-		"garrysmod/bin/lua_shared.so",
-		"bin/linux32/lua_shared.so",
-		"bin/linux32/lua_shared_client.so",
+fn find_lua_shared() -> Option<Library> {
+	unsafe {
+		open_library!("garrysmod/bin/lua_shared_srv.so")
+			.or_else(|| open_library!("garrysmod/bin/lua_shared.so"))
+			.or_else(|| open_library!("bin/linux32/lua_shared.so"))
+			.or_else(|| open_library!("bin/linux32/lua_shared_client.so"))
 	}
 }
 
 #[cfg(fg_linux64)]
-fn lua_path() -> Option<&'static Path> {
-	find_lua_path! {
-		"bin/linux64/lua_shared.so",
-		"bin/linux64/lua_shared_client.so",
+fn find_lua_shared() -> Option<Library> {
+	unsafe {
+		open_library!("bin/linux64/lua_shared.so")
+			.or_else(|| open_library!("bin/linux64/lua_shared_client.so"))
 	}
 }
 
 #[cfg(fg_unsupported)]
-fn lua_path() -> Option<&'static Path> {
+fn find_lua_shared() -> Option<Library> {
 	compile_error!("this platform isn't supported");
 	None
 }
 
 /// The `lua_shared` library.
 pub static LUA_SHARED: LazyLock<Library> = LazyLock::new(|| {
-	lua_path()
-		.and_then(|path| unsafe { Library::new(path).ok() })
-		.expect("failed to open lua_shared")
+	find_lua_shared().expect("failed to open lua_shared")
 });
