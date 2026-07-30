9b0370dc256b52ebacda8e6b8cc3f1bf873a3181
Dragoteryx <dragoteryx@live.fr>
2026-07-30 19:59:43 +0200

    await the async-io thread

--- flatgrass/src/task.rs
+++ flatgrass/src/task.rs
@@ -1,7 +1,11 @@
 use crate::lua::Lua;
 use async_channel::{Sender, bounded};
 use async_executor::LocalExecutor;
+use async_io::Timer;
+use std::cell::RefCell;
 use std::future::IntoFuture;
+use std::thread::JoinHandle;
+use std::time::Duration;
 
 #[doc(inline)]
 pub use async_executor::{FallibleTask, Task};
@@ -10,16 +14,21 @@ pub fn spawn<F: IntoFuture + 'static>(future: F) -> Task<F::Output> {
 	Lua::get(|lua| lua.async_runtime().spawn(future))
 }
 
+pub async fn sleep(duration: Duration) {
+	Timer::after(duration).await;
+}
+
 #[derive(Debug)]
 pub struct Runtime {
 	executor: LocalExecutor<'static>,
+	thread: RefCell<Option<JoinHandle<()>>>,
 	sender: Sender<()>,
 }
 
 impl Runtime {
 	pub(crate) fn new() -> Self {
 		let (sender, receiver) = bounded(1);
-		std::thread::spawn(move || {
+		let thread = std::thread::spawn(move || {
 			async_io::block_on(async {
 				let _ = receiver.recv().await;
 			});
@@ -27,6 +36,7 @@ impl Runtime {
 
 		Self {
 			executor: LocalExecutor::new(),
+			thread: RefCell::new(Some(thread)),
 			sender,
 		}
 	}
@@ -42,5 +52,9 @@ impl Runtime {
 
 	pub fn shutdown(&self) {
 		let _ = self.sender.try_send(());
+		let thread = self.thread.borrow_mut().take();
+		if let Some(thread) = thread {
+			let _ = thread.join();
+		}
 	}
 }
